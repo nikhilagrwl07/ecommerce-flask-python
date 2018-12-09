2b7ae50f4c4b85908b56b66e39efa9196d60e3ab
Akanksha <anegi@luc.edu>
2018-12-08 22:01:03 -0600

    Revert "cart changes"
    This reverts commit c2209b33a016daa6f929de89e49ebcea9f1077f5.

--- ecommerce/forms.py
+++ ecommerce/forms.py
@@ -16,11 +16,6 @@ from ecommerce.models import *
 from sqlalchemy import select, func, Integer, Table, Column, MetaData
 from flask import session, request
 import hashlib, os
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-
-
 
 
 def getAllProducts():
@@ -102,13 +97,11 @@ def extractAndPersistUserDataFromForm(request):
     db.session.commit()
     return "Registered Successfully"
 
-
 def isUserLoggedIn():
     if 'email' not in session:
         return False
     else:
         return True
-
 
 def extractAndPersistKartDetails(productId):
     userId = User.query.with_entities(User.userid).filter(User.email == session['email']).first()
@@ -117,7 +110,7 @@ def extractAndPersistKartDetails(productId):
     quantity = Cart.query.with_entities(Cart.quantity).filter_by(**kwargs).first()
 
     if quantity is not None:
-        cart = Cart(userid=userId, productid=productId, quantity=quantity[0] + 1)
+        cart = Cart(userid=userId, productid=productId, quantity=quantity[0]+1)
     else:
         cart = Cart(userid=userId, productid=productId, quantity=1)
 
@@ -153,166 +146,3 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
-
-#START CART MODULE
-#Gets products in the cart-Akanksha
-def getusercartdetails():
-    userId = User.query.with_entities(User.userid).filter(User.email == session['email']).first()
-
-    productsincart = Product.query.join(Cart, Product.productid == Cart.productid) \
-        .add_columns(Product.productid, Product.product_name, Product.discounted_price, Cart.quantity) \
-        .add_columns(Product.discounted_price * Cart.quantity).filter(
-        Cart.userid == userId)
-    totalsum = 0
-
-    for row in productsincart:
-        totalsum += row[5]
-
-    tax = ("%.2f" % (.06 * float(totalsum)))
-
-    totalsum = float("%.2f" % (1.06 * float(totalsum)))
-    return (productsincart, totalsum, tax)
-
-#Removes products from cart when user clicks remove-Akanksha
-def removeProductFromCart(productId):
-    userId = User.query.with_entities(User.userid).filter(User.email == session['email']).first()
-    userId = userId[0]
-    kwargs = {'userid': userId, 'productid': productId}
-    cart = Cart.query.filter_by(**kwargs).first()
-    if productId is not None:
-        db.session.delete(cart)
-        db.session.commit()
-        flash("Product has been removed from cart !!")
-    else:
-        flash("failed to remove Product cart please try again !!")
-    return redirect(url_for('cart'))
-
-#flask form for checkout details-Akanksha
-class checkoutForm(FlaskForm):
-    fullname = StringField('Full Name',
-                           validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    address = TextAreaField('address',
-                            validators=[DataRequired()])
-    city = StringField('city',
-                       validators=[DataRequired(), Length(min=2, max=20)])
-    state = StringField('state',
-                        validators=[DataRequired(), Length(min=2, max=20)])
-    zip = StringField('zip',
-                      validators=[DataRequired(), Length(min=2, max=6)])
-    cctype = RadioField('cardtype')
-    cardname = StringField('cardnumber',
-                           validators=[DataRequired(), Length(min=12, max=12)])
-    ccnumber = StringField('Credit card number',
-                           validators=[DataRequired()])
-
-    expmonth = StringField('Exp Month',
-                           validators=[DataRequired(), Length(min=12, max=12)])
-    expyear = StringField('Expiry Year',
-                          validators=[DataRequired(), Length(min=4, max=4)])
-    cvv = StringField('CVV',
-                      validators=[DataRequired(), Length(min=3, max=4)])
-    submit = SubmitField('MAKE PAYMENT')
-
-#Gets form data for the sales transaction-Akanksha
-def extractOrderdetails(request, totalsum):
-    fullname = request.form['FullName']
-    email = request.form['email']
-    address = request.form['address']
-    phone=request.form['phone']
-    city = request.form['city']
-    state = request.form['state']
-    zipcode = request.form['zip']
-    cctype=request.form['cardtype']
-    ccnumber = request.form['cardnumber']
-    cardname = request.form['cardname']
-    expmonth = request.form['expmonth']
-    expyear = request.form['expyear']
-    cvv = request.form['cvv']
-    orderdate = datetime.utcnow()
-    userId = User.query.with_entities(User.userid).filter(User.email == session['email']).first()
-    userId = userId[0]
-    order = Order(order_date=orderdate, total_price=totalsum, userid=userId)
-    db.session.add(order)
-    db.session.flush()
-    db.session.commit()
-
-    orderid = Order.query.with_entities(Order.orderid).filter(Order.userid == userId).order_by(Order.orderid.desc()).first()
-
-    # add details to ordered;
-    #  products table
-    addOrderedproducts(userId,orderid)
-    #add transaction details to the table
-    updateSalestransaction(totalsum,ccnumber,orderid,cctype)
-
-    #remove ordered products from cart after transaction is successful
-    removeordprodfromcart(userId)
-    #sendtextconfirmation(phone,fullname,orderid)
-    return (email, fullname,orderid,address,fullname,phone)
-
-# adds data to orderdproduct table-Akanksha
-def addOrderedproducts(userId,orderid):
-
-    cart=Cart.query.with_entities(Cart.productid,Cart.quantity).filter(Cart.userid == userId)
-
-    for item in cart:
-        orderedproduct=OrderedProduct(orderid=orderid, productid=item.productid, quantity=item.quantity)
-        db.session.add(orderedproduct)
-        db.session.flush()
-        db.session.commit()
-
-#removes all sold products from cart for the user-Akanksha
-def removeordprodfromcart(userId):
-    userid= userId
-    db.session.query(Cart).filter(Cart.userid == userid).delete()
-    db.session.commit()
-
-#adds sales transaction-Akanksha
-def updateSalestransaction(totalsum,ccnumber,orderid,cctype):
-    salesTransaction = SaleTransaction(orderid=orderid, transaction_date=datetime.utcnow(), amount=totalsum,cc_number=ccnumber,cc_type=cctype,response="success")
-    db.session.add(salesTransaction)
-    db.session.flush()
-    db.session.commit()
-
-#sends email for order confirmation-Akanksha
-def sendEmailconfirmation(email, username,ordernumber):
-    msg = MIMEMultipart()
-    sitemail = "stargadgets@engineer.com"
-    msg['Subject'] = "Your Order has been placed for " + username
-    msg['From'] = sitemail
-    msg['To'] = email
-    text = "Hello!\nThank you for shopping with us"
-    html = """\
-    <html>
-      <head></head>
-      <body>
-        <p><br>
-           Please stay tuned for more fabulous offers and gadgets.You can visit your account for more details on this order.<br> 
-           <br>Please write to us at <u>shopoholic@usa.com</u> for any assistance.</br>
-           <br></br>
-           <br></br>
-           Thank you!
-           <br></br>
-           Shopoholic Team          
-        </p>
-      </body>
-    </html>
-    """
-    msg1 = MIMEText(text, 'plain')
-    msg2 = MIMEText(html, 'html')
-    msg.attach(msg1)
-    msg.attach(msg2)
-    server = smtplib.SMTP(host='smtp.mail.com', port=587)
-    server.connect('smtp.mail.com', 587)
-    # Extended Simple Mail Transfer Protocol (ESMTP) command sent by an email server to identify itself when connecting to another email.
-    server.ehlo()
-    #upgrade insecure connection to secure
-    server.starttls()
-    server.ehlo()
-    server.login("stargadgets@engineer.com", "stargadget@123")
-    server.ehlo()
-    server.sendmail(sitemail, email, msg.as_string())
-    server.quit()
-
-#END CART MODULE
--- ecommerce/static/js/validateForm.js
+++ ecommerce/static/js/validateForm.js
@@ -1,12 +1,122 @@
 function validate() {
+    var result = validatePassword();
+    return result;
+}
+
+function validatePassword() {
     var pass = document.getElementById("password").value;
     var cpass = document.getElementById("cpassword").value;
     if (pass == cpass) {
-        return true;
+        return validateEmailAddress()
     } else {
         alert("Passwords do not match");
+        document.getElementById("cpassword").focus();
         return false;
     }
 }
 
+function validateEmailAddress() {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(document.getElementById("email").value)) {
+        return validatePhoneNumber()
+    }
+    alert("You have entered an invalid email address!")
+    document.getElementById("email").focus();
+    return (false)
+}
 
+// Allowed Format
+// (123) 456-7890
+// 123-456-7890
+// 123.456.7890
+// 1234567890
+function validatePhoneNumber() {
+    if (/^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/.test(document.getElementById("phone").value)) {
+        return (true)
+    }
+    alert("You have entered an invalid phone number!")
+    document.getElementById("phone").focus();
+    return (false)
+}
+
+
+var countryStateInfo = {
+    "USA": {
+        "California": {
+            "Los Angeles": ["90001", "90002", "90003", "90004"],
+            "San Diego": ["92093", "92101"]
+        },
+        "Texas": {
+            "Dallas": ["75201", "75202"],
+            "Austin": ["73301", "73344"]
+        }
+    },
+    "India": {
+        "Assam": {
+            "Dispur": ["781005"],
+            "Guwahati": ["781030", "781030"]
+        },
+        "Gujarat": {
+            "Vadodara": ["390011", "390020"],
+            "Surat": ["395006", "395002"]
+        }
+    }
+}
+
+
+
+window.onload = function () {
+
+    //Get html elements
+    var countySel = document.getElementById("countySel");
+    var stateSel = document.getElementById("stateSel");
+    var citySel = document.getElementById("citySel");
+    var zipSel = document.getElementById("zipSel");
+
+    //Load countries
+    for (var country in countryStateInfo) {
+        countySel.options[countySel.options.length] = new Option(country, country);
+    }
+
+    //County Changed
+    countySel.onchange = function () {
+
+        stateSel.length = 1; // remove all options bar first
+        citySel.length = 1; // remove all options bar first
+        zipSel.length = 1; // remove all options bar first
+
+        if (this.selectedIndex < 1)
+            return; // done
+
+        for (var state in countryStateInfo[this.value]) {
+            stateSel.options[stateSel.options.length] = new Option(state, state);
+        }
+    }
+
+    //State Changed
+    stateSel.onchange = function () {
+
+        citySel.length = 1; // remove all options bar first
+        zipSel.length = 1; // remove all options bar first
+
+        if (this.selectedIndex < 1)
+            return; // done
+
+        for (var city in countryStateInfo[countySel.value][this.value]) {
+            citySel.options[citySel.options.length] = new Option(city, city);
+        }
+    }
+
+    //City Changed
+    citySel.onchange = function () {
+        zipSel.length = 1; // remove all options bar first
+
+        if (this.selectedIndex < 1)
+            return; // done
+
+        var zips = countryStateInfo[countySel.value][stateSel.value][this.value];
+        for (var i = 0; i < zips.length; i++) {
+            zipSel.options[zipSel.options.length] = new Option(zips[i], zips[i]);
+        }
+    }
+
+}
